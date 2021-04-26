ef31e1fc2105576370eb9c9f1af7f186d493602a
patriziosbr <sbrozzi.patrizio@gmail.com>
2021-04-26 15:51:41 +0200

    pass ok

---
+++ js/pass.js
@@ -0,0 +1,10 @@
+var insertName = prompt('inserisci nome');
+var nome = insertName;
+
+var insertSur = prompt('inserisci cognome');
+var cognome = insertSur;
+
+var insertcolor = prompt('inserisci colore');
+var colore = insertcolor;
+
+document.getElementById("yourpass").innerHTML = ( nome + cognome + colore + 21 );
